d4f0d8ec98081146be0133706a59526a9cb3651f
dokdek <dokdek@gmail.com>
2023-03-22 12:23:52 -0400

    login & logout logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const router = useRouter();
+  const [auth, setAuth] = useState(false);
+
+  function logout() {
+    localStorage.clear();
+    setAuth(false);
+  }
+
+  useEffect(() => {
+    const getAccessToken = () => {
+      if (typeof window !== "undefined") return localStorage.getItem("token");
+    };
+    const token = getAccessToken();
+    if (token != undefined) {
+      setAuth(true);
+    } else {
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      setAuth(false);
+      router.push("/login");
+    }
+  });
+  if (auth == true) {
+    console.log("authed");
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">Search Page</h1>
+        <button
+          className="font-bold py-2 px-4 rounded-full bg-green-500"
+          type="button"
+          onClick={logout}
+        >
+          Sign out
+        </button>
+      </div>
+    );
+  }
 }
---
+++ pages/login.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Login() {
+    const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  async function signIn() {
+    console.log("Attempting logging in as " + username);
+    const loginJSON = {
+        username: username,
+        password: password
+    }
+    const response = await fetch('http://localhost:3001/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginJSON)
+    });
+    if(response.status == 200){
+        const token = await response.json();
+        console.log(token);
+        localStorage.setItem("token", token);
+        localStorage.setItem("username", username);
+        router.push("/");
+    }else{
+        setLoginError(true);
+    }
+  }
+
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <div className="mb-4">
+        <label className="block text-grey-darker text-sm font-bold mb-2">
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+      </div>
+      <div className="mb-6">
+        <label className="block text-grey-darker text-sm font-bold mb-2">
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+          id="password"
+          type="password"
+          placeholder="******************"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <p className="text-red text-xs italic" hidden={!loginError}>Error logging in, please try again</p>
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue hover:bg-blue-dark font-bold py-2 px-4 rounded"
+          type="button"
+          onClick={signIn}
+        >
+          Sign In
+        </button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </div>
+  );
+}
